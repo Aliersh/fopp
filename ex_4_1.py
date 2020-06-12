ca563ff5a2eda9dfb55516f1375d1af69749915e
Aliersh <ariel.diaz.borquez@gmail.com>
2020-06-11 23:40:33 -0400

    Add Chapter 4 files

---
+++ ex_4_1.py
@@ -0,0 +1,7 @@
+'''Use a for statement to print 10 random numbers.'''
+
+import random
+
+for i in range(10):
+    rn=random.random()
+    print(rn)
---
+++ ex_4_2.py
@@ -0,0 +1,7 @@
+'''Repeat the above exercise but this time print 10 random numbers between 25 and 35.'''
+
+import random
+
+for i in range(10):
+    rn=random.randrange(25,36)
+    print(rn)
