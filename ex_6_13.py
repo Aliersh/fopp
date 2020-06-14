750714f86de80387d01dc15a0556b04fd32d731f
Aliersh <ariel.diaz.borquez@gmail.com>
2020-06-13 21:49:16 -0400

    Add Chapter 6 files

---
+++ ex_6_13.py
@@ -0,0 +1,6 @@
+'''Create a variable called wrds and assign to it a list whose elements are the words in the string sent. 
+Do not worry about punctuation.'''
+
+sent = "The bicentennial for our university was in 2017"
+wrds = sent.split()
+print(wrds)
